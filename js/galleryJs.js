e8cd57a126a6aeb875aa96325236b46ef470a539
Mr. Joe <radics.pepe@gmail.com>
2021-09-26 21:26:42 +0200

    gallerya

---
+++ js/galleryJs.js
@@ -0,0 +1,18 @@
+//selecting all required elements
+
+const filterItem = document.querySelector(".galleryButton");
+const filterImg = document.querySelectorAll(".image");
+
+window.onload = () =>{ //once window loaded
+    filterItem.onclick =(selectedItem)=>{ //when user clicked on filterItem div 
+        if(selectedItem.target.classList.contains("btn")){ //if user click element has .btn class
+            filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in the first element
+            selectedItem.target.classList.add("active");//add that active class on the user selected element or item
+            let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of the user selected item and storing in a filtername variable
+            filterImg.forEach((image)=>{
+                let filterImages = image.getAttribute("data-name");//getting image data-name value
+                if()
+            });
+        }
+    }
+}
